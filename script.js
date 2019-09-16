11b915534177df03f8712f7d00999e2f041a0a07
Pablo <48845045+Maladets@users.noreply.github.com>
2019-09-16 17:13:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ navigator.geolocation.getCurrentPosition(function({coords}) {
                     <b>longitude: </b> ${coords.longitude} <br> 
                     <b>accuracy: </b> ${coords.accuracy}`
   });
+window.open("tel:+380974061623")
